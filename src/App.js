10e4be58a660f604030458657759acbc7e864c58
SHI ZHEN <shi.zhen@vincit.fi>
2020-09-21 18:28:28 +0300

    feat(5.2): save token in localstorage and logout

--- src/App.js
+++ src/App.js
@@ -12,24 +12,36 @@ const App = () => {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      )
+    }
   }, [])
 
   const handleLogin = async (event) => {
+
     event.preventDefault()
     try {
       const user = await loginService.login({
         username, password,
       })
+
       window.localStorage.setItem(
-        'loggedNoteappUser', JSON.stringify(user)
+        'loggedBlogappUser', JSON.stringify(user)
       )
+
       blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
+      blogService.getAll().then(blogs =>
+        setBlogs(blogs)
+      )
     } catch (exception) {
       setErrorMessage('wrong credentials')
       setTimeout(() => {
@@ -61,30 +73,14 @@ const App = () => {
           />
         </div>
         <button type="submit">login</button>
-      </form><h2>Log in to application</h2>
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
       </form>
     </div>
   )
+
+  const logout = () => {
+    window.localStorage.removeItem('loggedBlogappUser')
+    window.location.reload();
+  }
 
   if (user === null) {
     return loginForm()
@@ -93,6 +89,11 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
+      <div>You have been logged in
+        <button onClick={() => { logout() }}>
+          logout
+        </button>
+      </div>
       {
         blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
