8705f5ac78a75921fbb347b43d4009c517bf2ca8
SHI ZHEN <shi.zhen@vincit.fi>
2020-09-26 18:16:52 +0300

    feat(5.6-5.7): Toggle detail views

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+  const [isOpen, setIsOpen] = useState(false)
   console.log("blog", blog)
   if (blog) {
-    return < div >
-      {blog.title} {blog.author}
-    </div >
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title}
+          <button onClick={() => setIsOpen((prevState) => !prevState)}>
+            {isOpen ? "hide" : "view"}
+          </button>
+        </div>
+        {isOpen &&
+          <div>
+            {blog.author}
+          </div>
+        }
+      </div>
+    )
   }
   return null
 }
